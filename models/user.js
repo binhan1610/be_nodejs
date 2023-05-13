f1e7d31545f79d63d5370e4424d9e412b8f71ad6
binhan1610 <dangbinhan1610@gmail.com>
2023-05-13 21:50:45 +0700

    heh11

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/mindx', { useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://dangbinhan1610:binhan1012001@cluster0.zphxwo8.mongodb.net/mindx', { useNewUrlParser: true,useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
