0f37d393554705a894d4780b702e729ae774bd5a
binhan1610 <dangbinhan1610@gmail.com>
2023-05-16 21:44:10 +0700

    binhan

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { userRouter } = require('./routes/user')
 const jwt = require('jsonwebtoken')
 const { users, userModel } = require('./models/user')
 const bcrypt = require('bcrypt')
-
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 const authenticationCheck = async (req, res, next) => {
@@ -33,42 +33,52 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    //check trong db
-    const user = await userModel.findOne({ username })
-    //nếu có user thì trả token , còn không thì trả lỗi
-    if (user && bcrypt.compareSync(password, user.password)) {
-        const token = jwt.sign({ username: username }, '123@lol')
-        // Tra token cho client
-        res.send({ token: token })
-    } else {
-        res.send('khong tim thay')
+    try {
+        const { username, password } = req.body
+        // kiểm tra trong db
+        const user = await userModel.findOne({ username })
+        
+        // nếu có người dùng thì tạo token, ngược lại trả về lỗi
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign({ username: username }, '123@lol')
+            // Tra token cho client
+            res.send(token)
+        } else {
+            console.log(req.body);
+            res.send('khong tim thay')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Đã có lỗi xảy ra');
     }
-})
+});
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body
-    // check trùng username trong db 
-    const existringUser = await userModel.findOne({ username })
-    // nếu trùng thì không cho tạo , nếu không trùng thì tạo user 
-    // tim user có usename == req.body.username
-    // nếu tồn tại thì res.send('user da ton tại )
-    // nếu ko thì create
-    if (existringUser) {
-        res.send('user ton tại')
-    } else {
-        const salt = bcrypt.genSaltSync(10);
-        const hashPassword = bcrypt.hashSync(password,salt)
-        const user = await userModel.create({ username, password: hashPassword, role: ['user    '] })
-        res.send(user)
+    try {
+        // check trùng username trong db 
+        const existingUser = await userModel.findOne({ username })
+        // nếu trùng thì không cho tạo , nếu không trùng thì tạo user 
+        // tim user có usename == req.body.username
+        // nếu tồn tại thì res.send('user da ton tại )
+        // nếu ko thì create
+        if (existingUser) {
+            res.send('user đã tồn tại')
+        } else {
+            const salt = bcrypt.genSaltSync(10);
+            const hashPassword = bcrypt.hashSync(password,salt)
+            const user = await userModel.create({ username, password: hashPassword, role: ['user'] })
+            res.send(user)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Có lỗi xảy ra!')
     }
-
 })
-
 app.put('/update', async (req, res) => {
     const { username, password } = res.body
 })
 
-app.listen(3000)
+app.listen(3001)
 console.log('Server running')
 module.exports = { app }
